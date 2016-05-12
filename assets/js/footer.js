f96518204c74a8d0f681729262e026e82432ba78
Scott <scottcurrell@gmail.com>
2016-05-11 21:23:56 -0600

    Initial Commit

---
+++ assets/js/footer.js
@@ -0,0 +1,6 @@
+		// Copyright span
+var copyright = document.getElementsByClassName('copyright')[0],
+		// Current year
+		currentYear = new Date().getFullYear();
+// Set copyright span to current year
+copyright.innerHTML = currentYear;
