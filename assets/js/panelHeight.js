4e1204e240fdee8daeb6268ff3c730efcf1beeb0
Scott <scottcurrell@gmail.com>
2016-05-24 21:12:52 -0600

    Panel accent & img alignment.

--- assets/js/panelHeight.js
+++ assets/js/panelHeight.js
@@ -3,8 +3,12 @@
  * @param  {Object} accent : js-panel-accent
  */
 function panelHeight(accent) {
-    // .js-accent-panel
-    var accentPanel = $('.' + accent);
+    
+        // .js-accent-panel
+    var accentPanel = $('.' + accent),
+        // margin-left of accentPanel
+        accentPanelMargin = Number(accentPanel.css('margin-left')
+                                              .split('px')[0]);
 
     for (var i = 0; i < accentPanel.length; i++) {
             // find sibling .js-content-panel
@@ -16,6 +20,6 @@ function panelHeight(accent) {
         // set .js-accent-panel height to tha of .js-content-panel
         $(accentPanel[i]).height(contentPanelHeight);
         // apply neg margin to .js-contetn-panel
-        $(contentPanel).css('margin-top', -1 * (contentPanelHeight + 25) + 'px');
+        $(contentPanel).css('margin-top', -1 * (contentPanelHeight + accentPanelMargin) + 'px');
     }
  }
