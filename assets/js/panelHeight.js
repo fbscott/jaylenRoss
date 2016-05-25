99087d3b57706710c018e07e5aa5076a136bd28e
Scott <scottcurrell@gmail.com>
2016-05-25 00:13:24 -0600

    Add book images and book panels.

--- assets/js/panelHeight.js
+++ assets/js/panelHeight.js
@@ -3,7 +3,7 @@
  * @param  {Object} accent : js-panel-accent
  */
 function panelHeight(accent) {
-    
+
         // .js-accent-panel
     var accentPanel = $('.' + accent),
         // margin-left of accentPanel
@@ -19,7 +19,14 @@ function panelHeight(accent) {
 
         // set .js-accent-panel height to tha of .js-content-panel
         $(accentPanel[i]).height(contentPanelHeight);
-        // apply neg margin to .js-contetn-panel
-        $(contentPanel).css('margin-top', -1 * (contentPanelHeight + accentPanelMargin) + 'px');
+
+        // alternate positive/negative margin-top
+        if(i % 2 === 0) {
+            // apply neg margin to .js-contetn-panel
+            $(contentPanel).css('margin-top', -1 * (contentPanelHeight + accentPanelMargin) + 'px');
+        } else {
+            // apply neg margin to .js-contetn-panel
+            $(contentPanel).css('margin-top', -1 * (contentPanelHeight - accentPanelMargin) + 'px');
+        }
     }
  }
